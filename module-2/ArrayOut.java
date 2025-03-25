3a7c4b23feed473e96e58113c22b051f1595ea2b
Pandma13 <mbullard3@gmail.com>
2025-03-25 12:48:00 -0500

    Pushing initial commit for module-2

---
+++ module-2/ArrayOut.java
@@ -0,0 +1,17 @@
+// Megan Wheeler
+// Assignment 2
+// 25 March 2025
+
+// Follow the directions below:
+// 1. Write a program that stores:
+//      a. An array of five random integers.
+//      b. An array of five random double values.
+// 2. Write the data in a file titled [yourname] datafile.dat.
+// 3. If there is no file, the file will be created.
+// 4. If there is a file, the data will be appended.
+// 5. Write a second program that will read the file and display the data.
+// 6. Test the code to ensure the code functions correctly.
+
+public class ArrayOut {
+    
+}
