a58685b2b59f8a3e8520606cb659474320a25a89
Pandma13 <mbullard3@gmail.com>
2025-04-05 14:54:30 -0500

    Initial push for module-4

---
+++ module-4/TimeToTraverseLinkedList.java
@@ -0,0 +1,16 @@
+// Megan Wheeler
+// Assignment 4
+// 5 April 2025
+
+// Write a test program that stores 50,000 integers in LinkedList and test the time
+// to traverse the list using an iterator vs. using the get(index) method. 
+
+// 1.  Test your program storing first 50,000 and then 500,000 integers.
+// 2.  After completing this program and having tested both values, in your comments,
+//     explain the results and discuss the time taken using both values and their
+//     difference with the get(index) approach.
+// 3.  Write test code that ensures the code functions correctly.
+
+public class TimeToTraverseLinkedList {
+    
+}
