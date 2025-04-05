12d7133b733343164d536160c455e7002c2cc4aa
Pandma13 <mbullard3@gmail.com>
2025-04-05 10:35:22 -0500

    Initial push for module-3

---
+++ module-3/IntArrayListDeduplicator.java
@@ -0,0 +1,26 @@
+// Megan Wheeler
+// Assignment 3
+// 5 April 2025
+
+// 1. Fill an ArrayList with 50 random values from 1 to 20.
+// 2. Return a new ArrayList that contains all original values and no duplicates
+//     from the original ArrayList.
+// 3. Use public static <E> ArrayList<E> removeDuplicates(ArrayList<E> list)
+import java.util.ArrayList;
+
+public class IntArrayListDeduplicator {
+    public static void main(String[] args) {
+        
+    }
+
+    // build and populate ArrayList with 50 random int elements from 1-20
+    public static void randomIntArrayList(){
+
+    }
+
+    // pass random ArrayList to remove duplicates and return updated ArrayList
+    public static <E> ArrayList<E> removeDuplicates(ArrayList<E> list) {
+
+        return list;
+    }
+}
