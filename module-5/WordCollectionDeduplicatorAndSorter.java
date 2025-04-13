515662b51da3ab8966d557c3f06e2c3902302b8c
Pandma13 <mbullard3@gmail.com>
2025-04-13 18:26:11 -0500

    Final updates to module-5

--- module-5/WordCollectionDeduplicatorAndSorter.java
+++ module-5/WordCollectionDeduplicatorAndSorter.java
@@ -22,33 +22,58 @@ import java.util.TreeSet;
 public class WordCollectionDeduplicatorAndSorter {
     
     public static void main(String[] args) {
+        // initialize HashSet to store and call deduplicator method
+        HashSet<String> deduplicatedSet = wordCollectionDeduplicator();
+        // initialize TreeSet to store and call sorter method and with deduplicator method
+        TreeSet<String> sortedSet = wordCollectionSorter(deduplicatedSet);
 
+        // print results to user
+        System.out.println("---- Ascending Order Collection of Words ----");
+        System.out.println(sortedSet.toString());
+        System.out.println();
+        System.out.println("---- Descending Order Collection of Words ----");
+        System.out.println(sortedSet.descendingSet().toString());
     }
 
+    // method to read text file and add words to a set, returns a HashSet
     public static HashSet<String> wordCollectionDeduplicator() {
+        // initialize HashSet object
         HashSet<String> wordHashSet = new HashSet<>();
 
+        // try-catch for locating text file
         try {
+            // initialize variable for text file
             File myCollection = new File("collection_of_words.txt");
+            // initialize scanner object to read text file
             try (Scanner myScanner = new Scanner(myCollection)) {
+                // loop through text file
                 while (myScanner.hasNextLine()) {
+                    // use scanner object to add words to hashset
                     wordHashSet.add(myScanner.nextLine());
-                    return wordHashSet;
                 }
+                return wordHashSet;
             }
 
         } catch (FileNotFoundException e) {
             System.out.println("The file could not be found.");
         }
+
         return wordHashSet;        
     }
 
-    public static TreeSet<String> wordCollectionSorter() {
-
+    // method to create sorted set from the deduplicated set
+    public static TreeSet<String> wordCollectionSorter(HashSet<String> i) {
+        // initialize TreeSet object
         TreeSet<String> wordTreeSet = new TreeSet<>();
-        wordTreeSet.addAll(wordHashSet);
-        
-        return null;
+
+        // try-catch for adding HashSet elements to TreeSet
+        try {
+            wordTreeSet.addAll(i);
+        } catch (ClassCastException e) {
+            System.out.println("The class could not be cast.");
+        }
+
+        return wordTreeSet;
         
     }
 }
