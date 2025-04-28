57d54b9cd44fd97d43bea047bd7758ab4931b0ad
Pandma13 <mbullard3@gmail.com>
2025-04-27 19:42:05 -0500

    pushing initial module-8

---
+++ module-8/MeganThreeThreads.java
@@ -0,0 +1,20 @@
+// Megan Wheeler
+// Assignment 8
+
+// Follow the directions below:
+
+// 1. Create a class titled [your first="" name="" here=""] ThreeThreads.
+// 2. In this class, you are to use three threads to output three types of
+//     characters to a text area for display.
+// 3. In the first thread, you are to output random letter characters such
+//     as a, b, c, d
+// 4. In the second thread, you are to output random number digits such as
+//     0, 1, 2, 3, 4, 5, 6, 7, 8, 9.
+// 5. In the third thread, you are to output random characters such as
+//     !, @, #, $, %, &, *
+// 6. Display a minimum of 10,000 for each of the three sets.
+// 7. Write test code that ensures all methods function correctly.
+
+public class MeganThreeThreads {
+    
+}
