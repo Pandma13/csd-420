9f0eaf53cd5c698f1d7cad8aad409345a7228063
Pandma13 <mbullard3@gmail.com>
2025-05-04 19:56:41 -0500

    pushing assignment 9

---
+++ module-9/Assignment_9_NEW/mavenproject1/src/main/java/com/csd_420/mavenproject1/Mavenproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.csd_420.mavenproject1;
+
+/**
+ *
+ * @author monke
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        CreateTable createTable = new CreateTable();
+        InsertData InsertData = new InsertData();
+        Select5.main(args);
+    }
+}
